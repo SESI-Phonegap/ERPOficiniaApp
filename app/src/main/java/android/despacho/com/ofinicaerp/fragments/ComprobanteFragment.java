32b4702bf3ffcf460aa2afb56fe4d1302126196d
Christian Gonzalez <sesimx@gmail.com>
2017-08-21 23:54:06 -0500

    Avance FragmentComprobante

--- app/src/main/java/android/despacho/com/ofinicaerp/fragments/ComprobanteFragment.java
+++ app/src/main/java/android/despacho/com/ofinicaerp/fragments/ComprobanteFragment.java
@@ -3,6 +3,7 @@ package android.despacho.com.ofinicaerp.fragments;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.despacho.com.ofinicaerp.activity.MenuPrincipal;
+import android.despacho.com.ofinicaerp.models.ModelComprobanteGastos;
 import android.despacho.com.ofinicaerp.models.ModelTipoGasto;
 import android.despacho.com.ofinicaerp.utils.Constants;
 import android.despacho.com.ofinicaerp.utils.UtilsDML;
@@ -11,18 +12,30 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.despacho.com.ofinicaerp.R;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ComprobanteFragment extends Fragment {
 
-
+    private ProgressDialog progressBar;
+    private EditText et_fechaFin;
+    private EditText et_fechaIni;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton_si, radioButton_no, radioButton_todos;
+    private Spinner spinner_tipo_comprobante, spinner_categoria;
+    private RecyclerView recyclerView;
+    private List<ModelComprobanteGastos>comprobanteGastosList;
 
     public ComprobanteFragment() {
         // Required empty public constructor
@@ -52,6 +65,21 @@ public class ComprobanteFragment extends Fragment {
     }
 
     public void init(){
+        et_fechaFin = (EditText) getActivity().findViewById(R.id.et_fechaFin);
+        et_fechaIni = (EditText) getActivity().findViewById(R.id.et_fechaIni);
+        radioGroup = (RadioGroup) getActivity().findViewById(R.id.radioGroup);
+        radioButton_todos = (RadioButton) getActivity().findViewById(R.id.radioButton_todos);
+        radioButton_si = (RadioButton) getActivity().findViewById(R.id.radioButton_si);
+        radioButton_no = (RadioButton) getActivity().findViewById(R.id.radioButton_no);
+        spinner_tipo_comprobante = (Spinner) getActivity().findViewById(R.id.spinner_tipo_comprobante);
+        spinner_categoria = (Spinner) getActivity().findViewById(R.id.spinner_categoria);
+        recyclerView = (RecyclerView) getActivity().findViewById(R.id.recycler_view_comprobantes);
+        progressBar = new ProgressDialog(getActivity());
+        progressBar.setMessage("Cargando...");
+        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressBar.setCancelable(false);
+        progressBar.setCanceledOnTouchOutside(false);
+        progressBar.setIndeterminate(true);
 
         /*SELECT comprobante_gasto.id_comprobante_gasto,comprobante_gasto.fecha,comprobante_gasto.concepto,comprobante_gasto.monto,
                 comprobante_gasto.tipo_gasto_oxxo,comprobante_gasto.categoria,comprobante_gasto.tipo_comprobante
