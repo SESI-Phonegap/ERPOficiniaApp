529264be21fc4b664f429c7b0fe5f6740f8e9b27
Christian Gonzalez <sesimx@gmail.com>
2017-08-31 22:03:27 -0500

    Avance Update Honorarios

--- app/src/main/java/android/despacho/com/ofinicaerp/activity/FormHonorario.kt
+++ app/src/main/java/android/despacho/com/ofinicaerp/activity/FormHonorario.kt
@@ -5,16 +5,24 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.RadioGroup
 import kotlinx.android.synthetic.main.activity_form_honorario.btn_guardar
 import kotlinx.android.synthetic.main.activity_form_honorario.et_monto
 import kotlinx.android.synthetic.main.activity_form_honorario.spinner_ano
 import kotlinx.android.synthetic.main.activity_form_honorario.spinner_mes
-import kotlinx.android.synthetic.main.activity_form_honorario.spinner_status
+import kotlinx.android.synthetic.main.activity_form_honorario.radioGroup_status
+import kotlinx.android.synthetic.main.activity_form_honorario.radioButton_abono
+import kotlinx.android.synthetic.main.activity_form_honorario.radioButton_noPagado
+import kotlinx.android.synthetic.main.activity_form_honorario.radioButton_pagado
 import kotlinx.android.synthetic.main.activity_form_honorario.tv_cliente
 import kotlinx.android.synthetic.main.activity_form_honorario.tv_id_hono
+import kotlinx.android.synthetic.main.activity_form_honorario.img_back
+import kotlinx.android.synthetic.main.activity_form_nomina.*
 import java.util.*
 
-class FormHonorario : AppCompatActivity() {
+class FormHonorario : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
+
+    private var iStatus: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,15 +47,31 @@ class FormHonorario : AppCompatActivity() {
         spinner_ano.adapter = ArrayAdapter(application,R.layout.row_spinner_item,arrayAno)
         spinner_mes.adapter = ArrayAdapter(application,R.layout.row_spinner_item,meses)
 
-
         tv_id_hono.text = getString(R.string.numRegistro,idHono)
         tv_cliente.text = cliente
-        spinner_mes.setSelection(idMes.toInt())
+        spinner_mes.setSelection(idMes.toInt()-1)
+
+        radioGroup_status.setOnCheckedChangeListener(this)
 
 
 
+        imgBack.setOnClickListener({
+            onBackPressed()
+        })
 
+    }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
 
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        when(checkedId){
+            R.id.radioButton_pagado -> { iStatus = 1 }
+
+            R.id.radioButton_abono -> {  iStatus = 4 }
+
+            R.id.radioButton_noPagado -> { iStatus = 2 }
+        }
     }
 }
